f57bbc98b62e84db75106715aa880699f9392cb2
Jeff Lee <jcjl013@gmail.com>
2016-03-27 22:01:07 -0700

    Commit form definitions

---
+++ wsgi/djYlplines/main/forms.py
@@ -0,0 +1,22 @@
+"""
+ylplines - Clarity for Yelp
+Copyright (C) 2016  Jeff Lee
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+from django import forms
+
+
+class FrontSearchForm(forms.Form):
+    search_string = forms.CharField(label='Enter search terms', max_length=200)
