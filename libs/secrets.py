32d04f9174e4caaa8e4cf54e1bc5fd628471683e
Jeff Lee <jcjl013@gmail.com>
2016-03-27 22:08:32 -0700

    Cleanup

--- libs/secrets.py
+++ libs/secrets.py
@@ -4,12 +4,14 @@ __secrets = {
     'secret_key': 'a',
 }
 
+
 def getter(path):
     try:
         with open(path) as handle:
             return json.load(handle)
     except IOError:
         return __secrets
+
 
 def generator():
 
