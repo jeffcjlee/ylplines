8db2cdc2295484f690e5011a3d1a8fb039981006
Jeff Lee <jcjl013@gmail.com>
2016-04-01 13:21:53 -0700

    Get rid of stale code comments

--- wsgi/djYlplines/main/engine/search_businesses.py
+++ wsgi/djYlplines/main/engine/search_businesses.py
@@ -145,93 +145,20 @@ def bg_cb(session, response):
 
     sel = CSSSelector('div.review.review--with-sidebar:not(.js-war-widget) meta[itemprop="datePublished"]')
 
-    #publish_dates = [e.get('content') for e in sel(tree)]
     publish_dates = []
     for e in sel(tree):
         publish_date = e.get('content')
         publish_date = datetime.strptime(publish_date, '%Y-%m-%d').date()
         publish_dates.append(publish_date)
 
-
     sel = CSSSelector('p[itemprop="description"]')
     texts = []
-    #texts = [e.text for e in sel(tree)]
     for e in sel(tree):
         #text = e.replace(u'\xa0', u' ')
         texts.append(e)
     #texts.append(raw_text.get_text().replace(u'\xa0', u' '))
 
-
-
     response.data = [ids, ratings, publish_dates, texts]
-    """
-    strainer_start = time.clock()
-    strainer = SoupStrainer(class_='review review--with-sidebar')
-    strainer_end = time.clock()
-    souping_start = time.clock()
-    soup = BeautifulSoup(content, 'lxml', parse_only=strainer, from_encoding='UTF-8')
-    #soup = BeautifulSoup(content, 'lxml', from_encoding='UTF-8')
-    souping_end = time.clock()
-   # print("Internal souping duration: " + str(souping_end-souping_start))
-    # continue_review_retrieval = True
-    #print("Soup!::: ")
-    #print(soup.prettify())
-    raw_ids = []
-    raw_ratings = []
-    raw_publish_dates = []
-    raw_texts = []
-    id_find_start = time.clock()
-    raw_ids = soup.find_all('div', itemprop='review')
-    id_find_end = time.clock()
-    rating_find_start = time.clock()
-    raw_ratings = soup.find_all('meta', itemprop='ratingValue')
-    rating_find_end = time.clock()
-    date_find_start = time.clock()
-    raw_publish_dates = soup.find_all('meta', itemprop='datePublished')  # 2016-02-16
-    date_find_end = time.clock()
-    text_find_start = time.clock()
-    raw_texts = soup.find_all('p', itemprop='description')
-    text_find_end = time.clock()
-    #print(str(raw_ids))
-    #print(str(raw_ratings))
-    #print(str(raw_publish_dates))
-    #print(str(raw_texts))
-    ids = []
-    ratings = []
-    publish_dates = []
-    texts = []
-
-    forloop_start = time.clock()
-    for raw_id, raw_rating, raw_publish_date, raw_text in zip(raw_ids, raw_ratings, raw_publish_dates,
-                                                              raw_texts):
-        id = raw_id.attrs['data-review-id']
-        #print(str(id))
-        #if Review.objects.filter(id=id).exists(): # TODO TURN THESE BACK ON
-        #    continue
-
-        publish_date = raw_publish_date.attrs['content']
-        publish_date = datetime.strptime(publish_date, '%Y-%m-%d').date()
-
-        ids.append(id)
-        ratings.append(raw_rating.attrs['content'])
-        publish_dates.append(publish_date)
-        texts.append(raw_text.get_text().replace(u'\xa0', u' '))
-    forloop_end = time.clock()
-    #print(str(ids))
-    #print(str(ratings))
-    #print(str(publish_dates))
-    response.data = [ids, ratings, publish_dates, texts,
-                     strainer_end-strainer_start,
-                     souping_end-souping_start,
-                     id_find_end-id_find_start,
-                     rating_find_end-rating_find_start,
-                     date_find_end-date_find_start,
-                     text_find_end-text_find_start,
-                     forloop_end-forloop_start
-    ]
-    #print(str(response.data))
-    """
-
 
 
 def get_business_reviews(business, debug=False):
@@ -241,8 +168,8 @@ def get_business_reviews(business, debug=False):
     latest_review_date = None
     todays_date = datetime.today().date()
 
-    #if business.latest_review_pull and business.latest_review_pull + timedelta(days=7) >= todays_date:
-    #    return
+    if business.latest_review_pull and business.latest_review_pull + timedelta(days=200) >= todays_date:
+        return
 
     if Review.objects.filter(business_id=business.id).exists():
         latest_review_date = Review.objects.filter(business_id=business.id).latest('publish_date').publish_date
@@ -282,44 +209,18 @@ def get_business_reviews(business, debug=False):
 
     print('Begin response processing')
     print("Processing response (%s total)..." % num_requests, end="", flush=True)
-    strainer_dur = 0.0
-    souping_dur = 0.0
-    id_find_dur = 0.0
-    rating_find_dur = 0.0
-    date_find_dur = 0.0
-    text_find_dur = 0.0
-    forloop_dur = 0.0
 
     process_start = default_timer()
     for ctr, response in enumerate(responses, 1):
         print("%s..." % ctr, end="", flush=True)
         if response:
             if response.status_code == 200:
-                """
-                strainer_dur += response.data[4]
-                souping_dur += response.data[5]
-                id_find_dur += response.data[6]
-                rating_find_dur += response.data[7]
-                date_find_dur += response.data[8]
-                text_find_dur += response.data[9]
-                forloop_dur += response.data[10]
-                """
 
                 process_async_response(response, business, latest_review_date)
             else:
                 print("Retrieval unsuccessful, got a status code of: " + str(response.status_code))
     process_end = default_timer()
     print('\nResponse processing duration: %s seconds' % str(process_end-process_start))
-
-    """
-    print("Strainer init duration: " + str(strainer_dur) )
-    print("Souping duration: " +str(souping_dur))
-    print("id find duration: " +str(id_find_dur))
-    print("rating find duration: " +str(rating_find_dur))
-    print("date find duration: " +str(date_find_dur))
-    print("text find duration: " +str(text_find_dur))
-    print("forloop duration: " +str(forloop_dur))
-    """
 
     business.latest_review_pull = todays_date
     business.save()
@@ -336,62 +237,21 @@ def create_urls_list(business_url, num_reviews):
 
     for i in range(num_requests):
         start = i * NUM_REVIEWS_PER_PAGE
-        urls.append('http://google.com')
-        #urls.append(business_url + '?' + 'sort_by=date_desc' + '&' + 'start=' + str(start))
+        #urls.append('http://google.com')
+        urls.append(business_url + '?' + 'sort_by=date_desc' + '&' + 'start=' + str(start))
     return urls
 
 
 def process_async_response(response, business, latest_review_date):
-    """
-    content = response.content
-    strainer = SoupStrainer(class_='review review--with-sidebar')
-    soup = BeautifulSoup(content, 'lxml', parse_only=strainer, from_encoding='UTF-8')
-    #continue_review_retrieval = True
-
-    raw_ids = soup.find_all('div', itemprop='review')
-    raw_ratings = soup.find_all('meta', itemprop='ratingValue')
-    raw_publish_dates = soup.find_all('meta', itemprop='datePublished')  # 2016-02-16
-    raw_texts = soup.find_all('p', itemprop='description')
-
-    ids = []
-    ratings = []
-    publish_dates = []
-    texts = []
-
-    for raw_id, raw_rating, raw_publish_date, raw_text in zip(raw_ids, raw_ratings, raw_publish_dates,
-                                                              raw_texts):
-        id = raw_id.attrs['data-review-id']
-        if Review.objects.filter(id=id).exists():
-            continue
-
-        publish_date = raw_publish_date.attrs['content']
-        publish_date = datetime.strptime(publish_date, '%Y-%m-%d').date()
-        if latest_review_date is not None and publish_date < latest_review_date:
-            continue
-
-        ids.append(id)
-        ratings.append(raw_rating.attrs['content'])
-        publish_dates.append(publish_date)
-        texts.append(raw_text.get_text().replace(u'\xa0', u' '))
-
-    if not ids:
-        continue_review_retrieval = False
-        return continue_review_retrieval
-    """
     ids = response.data[0]
     ratings = response.data[1]
     publish_dates = response.data[2]
     texts = response.data[3]
 
-
-    #print(str(ids))
-    #print(str(ratings))
-    #print(str(publish_dates))
-    #print(str(texts))
     for id, rating, publish_date, text in zip(ids, ratings, publish_dates, texts):
         if not Review.objects.filter(id=id).exists():
             if latest_review_date is not None and publish_date < latest_review_date:
                 continue
             review = Review(id=id, business=business, rating=rating, publish_date=publish_date, text=text)
             review.save()
-    #return continue_review_retrieval
+
