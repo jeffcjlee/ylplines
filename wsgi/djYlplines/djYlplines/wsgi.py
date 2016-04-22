2fefaae946f4943d002544d41226060cc50b6f6a
Jeff Lee <jcjl013@gmail.com>
2016-04-22 15:55:01 -0700

    Implement New Relic for monitoring services

--- wsgi/djYlplines/djYlplines/wsgi.py
+++ wsgi/djYlplines/djYlplines/wsgi.py
@@ -21,9 +21,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-import os
+import newrelic.agent
+newrelic.agent.initialize()
 
+import os
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djYlplines.settings")
 
