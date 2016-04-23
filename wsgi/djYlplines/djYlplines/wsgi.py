4db6600318cb82003aca2d787b5dbe3e54c76ec3
Jeff Lee <jcjl013@gmail.com>
2016-04-22 17:06:48 -0700

    Attempt to get monitoring to work

--- wsgi/djYlplines/djYlplines/wsgi.py
+++ wsgi/djYlplines/djYlplines/wsgi.py
@@ -21,9 +21,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-import newrelic.agent
-newrelic.agent.initialize()
-
 import os
 from django.core.wsgi import get_wsgi_application
 
